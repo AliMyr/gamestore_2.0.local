d04dff6f37ba2d2d81522b3f1456dfa06e6f9bdb
AliMyr <rabo41ali@gmail.com>
2024-10-25 17:33:37 +0500

    Null

--- public/public/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset();  // Убираем все данные из сессии
-session_destroy();  // Разрушаем сессию
-
-// Перенаправляем на главную страницу
-header('Location: index.php');
-exit();
-?>
