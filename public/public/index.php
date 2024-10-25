c0e618396d7f611c4f9a0f8e1e0652a51fba51dc
AliMyr <rabo41ali@gmail.com>
2024-10-25 15:01:19 +0500

    init

--- public/public/index.php
+++ public/public/index.php
@@ -1,10 +1,29 @@
 <?php
-include '../includes/public/header.php';  // Подключаем шапку сайта
+include '../config/config.php';  // Подключение к базе данных
+include '../includes/public/header.php';  // Подключаем шапку
+
+// Получаем список игр из базы данных
+$stmt = $db->query("SELECT * FROM games ORDER BY created_at DESC");
+$games = $stmt->fetchAll();
 ?>
 
-<h1>Добро пожаловать в наш интернет-магазин игр!</h1>
-<p>Здесь вы найдёте множество интересных игр.</p>
+<h1>Каталог игр</h1>
+
+<?php if (count($games) > 0): ?>
+    <ul>
+        <?php foreach ($games as $game): ?>
+            <li>
+                <h2><?php echo htmlspecialchars($game['title']); ?></h2>
+                <p><?php echo htmlspecialchars($game['description']); ?></p>
+                <p>Цена: <?php echo htmlspecialchars($game['price']); ?> тенге</p>
+                <a href="game.php?id=<?php echo $game['id']; ?>">Подробнее</a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+<?php else: ?>
+    <p>Игр пока нет.</p>
+<?php endif; ?>
 
 <?php
-include '../includes/public/footer.php';  // Подключаем подвал сайта
+include '../includes/public/footer.php';  // Подключаем подвал
 ?>
