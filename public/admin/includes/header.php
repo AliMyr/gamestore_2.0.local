cb30fb53a7fd56678bbe4a9fc441168a80b6e28b
AliMyr <rabo41ali@gmail.com>
2024-10-26 16:03:23 +0500

    Null

--- public/admin/includes/header.php
+++
@@ -1,9 +0,0 @@
-<nav>
-    <a href="/admin/index.php">Главная админки</a> |
-    <a href="/admin/pages/add_game.php">Добавить игру</a> |
-    <a href="/admin/pages/manage_games.php">Управление играми</a> |
-    <a href="/admin/pages/manage_orders.php">Управление заказами</a>
-    <?php if (isset($_SESSION['user_id'])): ?>
-        | <a href="/public/pages/logout.php">Выйти</a>
-    <?php endif; ?>
-</nav>
